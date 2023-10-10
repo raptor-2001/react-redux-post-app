af6cc23847230f48161ab6f2fb58bb2d2360efdf
raptor-2001 <pratham007yadav@gmail.com>
2023-10-10 21:30:12 +0530

    post editform added

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -8,7 +8,20 @@ const initialState = [
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
-  reducers: {},
+  reducers: {
+    postAdded(state, action) {
+      state.push(action.payload)
+    },
+    postUpdated(state, action) {
+      const { id, title, content } = action.payload
+      const existingPost = state.find((post) => post.id === id)
+      if (existingPost) {
+        existingPost.title = title
+        existingPost.content = content
+      }
+    },
+  },
 })
 
+export const { postAdded, postUpdated } = postsSlice.actions
 export default postsSlice.reducer
